abc31c564ba1bdc24d3eb91d38f5a4819cd86d8a
Noah Rijkaard <noahrijkaard@gmail.com>
2022-12-11 19:43:38 +0800

    Finishing touches

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const gameBoard = (() => {
     };
     const drawBoard = () => {
         const cells = document.querySelectorAll('.cell');
+        
         cells.forEach((cell, index) => {
             cell.addEventListener('click', () => {
                 cell.innerHTML = Game.currentPlayer.symbol;
@@ -23,6 +24,7 @@ const gameBoard = (() => {
                 let col = index % 3;
                 board[row][col] = Game.currentPlayer.symbol;
                 cell.style.pointerEvents = 'none';
+                cell.classList.add('activated');
                 Game.remainingSpots--;
                 Game.checkWin();
               
@@ -36,7 +38,6 @@ const gameBoard = (() => {
             })
         });
     }
-        
 
     return { board, resetBoard, drawBoard };
 })();
@@ -46,20 +47,35 @@ const Player = (name, symbol) => {
     return { name, symbol };
 }
 
+const resetBoard = () => {
+    location.reload(true);
+}
+
+const changeName = () => {
+    const player1Input = document.querySelector('#player1');
+    const player2Input = document.querySelector('#player2');
+    Game.player1.name = player1Input.value;
+    Game.player2.name = player2Input.value;
+}
+
+
 // Game flow module
 const Game = (() => {
     // Alternate between players
-    const player1 = Player('Player 1', 'X');
-    const player2 = Player('Player 2', 'O');
+    let player1 = Player('Player 1', 'X');
+    let player2 = Player('Player 2', 'O');
+    const playerDisplay = document.querySelector('.player-display');
 
     // Set current player
     let currentPlayer = player1;
     let winner = false;
     let remainingSpots = 9;
 
+    playerDisplay.innerHTML = `${currentPlayer.name}'s turn`;
  
     function switchPlayer() {
         this.currentPlayer === player1 ? this.currentPlayer = player2 : this.currentPlayer = player1;
+        playerDisplay.innerHTML = `${this.currentPlayer.name}'s turn`;
     };
 
 
@@ -68,6 +84,7 @@ const Game = (() => {
         for (let i = 0; i < 3; i++) {
             if (gameBoard.board[i][0] === gameBoard.board[i][1] && gameBoard.board[i][0] === gameBoard.board[i][2] && gameBoard.board[i][0] !== '') {
                 this.winner = true;
+                playerDisplay.innerHTML = `${this.currentPlayer.name} wins!`;
                 return;
             }
         }
@@ -75,27 +92,30 @@ const Game = (() => {
         for (let i = 0; i < 3; i++) {
             if (gameBoard.board[0][i] === gameBoard.board[1][i] && gameBoard.board[0][i] === gameBoard.board[2][i] && gameBoard.board[0][i] !== '') {
                 this.winner = true;
+                playerDisplay.innerHTML = `${this.currentPlayer.name} wins!`;
                 return;
             }
         }
         // Check diagonals
         if (gameBoard.board[0][0] === gameBoard.board[1][1] && gameBoard.board[0][0] === gameBoard.board[2][2] && gameBoard.board[0][0] !== '') {
             this.winner = true;
+            playerDisplay.innerHTML = `${this.currentPlayer.name} wins!`;
             return;
         }
         if (gameBoard.board[0][2] === gameBoard.board[1][1] && gameBoard.board[0][2] === gameBoard.board[2][0] && gameBoard.board[0][2] !== '') {
             this.winner = true;
+            playerDisplay.innerHTML = `${this.currentPlayer.name} wins!`;
             return;
         }
     };
 
     const tie = () => {
-        alert('Tie!');
+        playerDisplay.innerHTML = 'Tie!';
         gameBoard.resetBoard();
     }
 
    
-    return { checkWin, switchPlayer,tie, currentPlayer, winner, remainingSpots };
+    return { checkWin, switchPlayer,tie, currentPlayer, winner, remainingSpots, player1, player2 };
 })();
 
 
